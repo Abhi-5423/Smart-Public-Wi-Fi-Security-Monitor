31c0610f475e6cb58b2fc061227896c74420cf97
Abhi-5423 <abhishekrajrock72@gmail.com>
2026-06-14 12:51:02 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Smart-Public-Wi-Fi-Security-Monitor/',
   plugins: [react()],
 });
